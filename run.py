c2e934496f46b0d8044c842af2a836021f45f972
Emese92 <gregor.emese@gmail.com>
2022-03-07 22:14:44 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of  numbers separated
+    via the terminal, which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
@@ -37,14 +37,12 @@ def get_sales_data():
     return sales_data
 
 
-
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -57,31 +55,24 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
+
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the list provided.
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet apdated successfully.\n")
-
-
-def jls_extract_def(surplus_data):
-    return surplus_data
-
-
-def jls_extract_def(surplus_data):
-    return jls_extract_def(surplus_data)
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
-
     The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplis indicates waste
-    - Negative sourplis indicates extra made when stock was sold out.
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
@@ -92,22 +83,39 @@ def calculate_surplus_data(sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
 
+    return surplus_data
 
-    return jls_extract_def(surplus_data)
 
 def get_last_5_entries_sales():
     """
-    Collects collumns of data from sales worksheet, collecting
+    Collects columns of data from sales worksheet, collecting
     the last 5 entries for each sandwich and returns the data
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
 
     columns = []
-    for ind in range(1,7):
+    for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
+
     return columns
+
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -116,12 +124,13 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_worksheet(sales_data,"sales")
-    new_sulprus_data = calculate_surplus_data(sales_data)
-    update_worksheet(new_sulprus_data, "surplus")
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
-print("Welcome to Love sandwiches Data Automation")
-#main()
-
-sales_columns = get_last_5_entries_sales()
+print("Welcome to Love Sandwiches Data Automation")
+main() 
